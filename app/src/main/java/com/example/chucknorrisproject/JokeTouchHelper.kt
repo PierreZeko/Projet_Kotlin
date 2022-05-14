d13abb13c193f732c49fd963569181f8a91f8c1a
PierreZeko <pierre.louis@edu.esiee.fr>
2022-05-14 17:18:24 +0200

    chuck norris project

--- app/src/main/java/com/example/chucknorrisproject/JokeTouchHelper.kt
+++ app/src/main/java/com/example/chucknorrisproject/JokeTouchHelper.kt
@@ -1,26 +1,31 @@
 package com.example.chucknorrisproject
 
+import android.util.Log
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 
-class JokeTouchHelper(
-    //private val onJokeRemoved: ??? ,
-    //private val onItemMoved: ???
-) : ItemTouchHelper(
+class JokeTouchHelper(private val onJokeRemoved: Boolean, private val onItemMoved: Boolean) : ItemTouchHelper(
     object : ItemTouchHelper.SimpleCallback(
         UP or DOWN,
         LEFT or RIGHT
     ) {
-        override fun onMove(
-            recyclerView: RecyclerView,
-            viewHolder: RecyclerView.ViewHolder,
-            target: RecyclerView.ViewHolder
-        ): Boolean = TODO("use it to reorder items")
+        val adapter = JokeAdapter(onBottomReach = {})
+
+        override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+            if (onItemMoved) {
+                Log.d("a joke has been moved", adapter.jokes[viewHolder.layoutPosition].value)
+                return true
+            }
+            else {
+                return false
+            }
+        }
 
         override fun onSwiped(viewHolder: RecyclerView.ViewHolder, swipeDir: Int) {
-
+            if (onJokeRemoved) {
+                Log.d("a joke has been removed", adapter.jokes[viewHolder.layoutPosition].value)
+                adapter.notifyItemRemoved(viewHolder.layoutPosition)
+            }
         }
     }
 )
-// val itemTouchHelper = ItemTouchHelper()
-//itemTouchHelper.attachToRecyclerView()
